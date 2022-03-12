32c86f986df08623df84b429643454afde17032d
tuanthanh201 <55332021+tuanthanh201@users.noreply.github.com>
2022-03-12 03:58:37 -0500

    Modified getDate so that it shows fromNow when time is less than 12 hours, otherwise it shows time like before

--- client/src/components/utils/getDate.js
+++ client/src/components/utils/getDate.js
@@ -1,7 +1,17 @@
 import moment from 'moment'
 
 const getDate = (dateString) => {
-  return moment(parseInt(dateString, 10)).format('MMMM Do, YYYY')
+  // check if date is 12h ago
+  const date = parseInt(dateString, 10)
+  const twentyFourHrInMs = 12 * 60 * 60 * 1000
+
+  const twentyFourHoursAgo = Date.now() - twentyFourHrInMs
+
+  if (date > twentyFourHoursAgo) {
+    return moment(date).fromNow()
+  } else {
+    return moment(date).format('HH:mm MMMM Do, YYYY')
+  }
 }
 
 export default getDate
