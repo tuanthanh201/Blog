542d4b71c853b77925c404f8862718d0ad881643
tuanthanh201 <55332021+tuanthanh201@users.noreply.github.com>
2022-03-15 03:41:02 -0400

    Set up subscription in ApolloProvider

--- client/src/Apollo/ApolloProvider.js
+++ client/src/Apollo/ApolloProvider.js
@@ -3,19 +3,38 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
+  split,
 } from '@apollo/client'
-
-const uri = 'http://localhost:4000/graphql'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
 
 const httpLink = createHttpLink({
-  uri,
+  uri: 'http://localhost:4000/graphql',
   credentials: 'include',
 })
 
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:4000/subscriptions',
+  })
+)
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  // link: link.concat(httpLink),
-  link: httpLink,
+  link: splitLink,
 })
 
 const CustomApolloProvider = ({ children }) => {
