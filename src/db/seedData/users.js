a2c628b83278af5ea23bda5282322308404c1cab
tuanthanh201 <55332021+tuanthanh201@users.noreply.github.com>
2022-04-23 03:10:16 -0400

    Update data for seeding

--- src/db/seedData/users.js
+++ src/db/seedData/users.js
@@ -7,7 +7,7 @@ const userData = [
     email: 'james@example.com',
     bio: '',
     password: '$2a$10$CxAtHi08jbEjZetJq2u4e.8XMxpdftZKLOcV/fY1QGBiCko6cX1H6',
-    createdAt: new Date('2022-04-02T23:02:31.453Z'),
+    createdAt: new Date(1648940551453),
     posts: [
       {
         _id: ObjectId('6248db1d57c84470d3107aac'),
@@ -19,7 +19,7 @@ const userData = [
         _id: ObjectId('62512f51d3e66386ecc4c500'),
       },
     ],
-    __v: 11,
+    __v: 164,
   },
   {
     _id: ObjectId('6248df9557c84470d3107b45'),
@@ -27,13 +27,13 @@ const userData = [
     email: 'mary@example.com',
     bio: '',
     password: '$2a$10$H2/GSHMf2JgBx0TDHI8fduUrM45WgZbbWnPvHgvlhZ3cecqCexvXa',
-    createdAt: new Date('2022-04-02T23:43:17.389Z'),
+    createdAt: new Date(1648942997389),
     posts: [
       {
         _id: ObjectId('6248e28157c84470d3107b4f'),
       },
     ],
-    __v: 1,
+    __v: 2,
   },
   {
     _id: ObjectId('6248e4cf57c84470d3107c07'),
@@ -41,7 +41,7 @@ const userData = [
     email: 'alina@example.com',
     bio: "Hello, it's nice to meet you! My name is Alina, and I love learning about marine life!",
     password: '$2a$10$MMV5z9DSyosxKAJ6tE7GMeBmmieQAOj4t1.sZ.w8kxWqnwk6dR/Ju',
-    createdAt: new Date('2022-04-03T00:05:35.238Z'),
+    createdAt: new Date(1648944335238),
     posts: [
       {
         _id: ObjectId('624905b157c84470d3107c0f'),
@@ -50,7 +50,7 @@ const userData = [
         _id: ObjectId('62491e182af5961bf4617a77'),
       },
     ],
-    __v: 2,
+    __v: 0,
   },
   {
     _id: ObjectId('6249105bb58655fb26a6c3fc'),
@@ -58,27 +58,27 @@ const userData = [
     email: 'Peter@example.com',
     bio: '',
     password: '$2a$10$l8bfI1V0ZGwrU3n/5.63/u3iXrs3vAz3/yqjZ5wHNR/Vl0KqEBnrW',
-    createdAt: new Date('2022-04-03T03:11:23.241Z'),
+    createdAt: new Date(1648955483241),
     posts: [
       {
         _id: ObjectId('6249321e2af5961bf4617c1a'),
       },
     ],
-    __v: 21,
+    __v: 0,
   },
   {
     _id: ObjectId('624932f72af5961bf4617c6f'),
     username: 'Jeremy',
     email: 'jeremy@example.com',
-    bio: '',
+    bio: 'Hi, my name is Jeremy. I love too cook!',
     password: '$2a$10$vvAuWJcY8WtJ0/TPDdOQ/OswHtzZXNjLM2RXruGxt3nnUEg9JjaVO',
-    createdAt: new Date('2022-04-03T05:39:03.247Z'),
+    createdAt: new Date(1648964343247),
     posts: [
       {
         _id: ObjectId('6249348a2af5961bf4617c79'),
       },
     ],
-    __v: 1,
+    __v: 0,
   },
   {
     _id: ObjectId('624935d42af5961bf4617d2a'),
@@ -86,21 +86,21 @@ const userData = [
     email: 'richard@example.com',
     bio: '',
     password: '$2a$10$Q.K6Xts3TArL6iJKEqfgS.sPSUiIvKndTtDGWY3WAkfIyxL/3kd8.',
-    createdAt: new Date('2022-04-03T05:51:16.983Z'),
+    createdAt: new Date(1648965076983),
     posts: [
       {
         _id: ObjectId('6249383d2af5961bf4617d31'),
       },
     ],
-    __v: 1,
+    __v: 0,
   },
   {
     _id: ObjectId('62539f5cd2acc41438c15c4d'),
     username: 'Arthur',
     email: 'arthur@example.com',
-    bio: '',
+    bio: "Hi there, my name is Arthur. It's nice to meet you!",
     password: '$2a$10$0lcRvch7z9G89L1OTRS7Tu3XOxit1Z0/gwZY9N0s1QOWVEtA.PzDK',
-    createdAt: new Date('2022-04-11T03:24:12.629Z'),
+    createdAt: new Date(1649647452629),
     posts: [
       {
         _id: ObjectId('6253a06ed2acc41438c15c59'),
@@ -112,7 +112,31 @@ const userData = [
         _id: ObjectId('6253a8fed893457dac8878e8'),
       },
     ],
-    __v: 27,
+    __v: 46,
+  },
+  {
+    _id: ObjectId('625fbda44aa3cc38026b7569'),
+    username: 'Thomas',
+    email: 'thomas@example.com',
+    bio: '',
+    password: '$2a$10$bZcOyijOlaDfK4iPMlUktOXUgni9yQ317BSxjJOvPUla1g0IZCkta',
+    createdAt: new Date(1650441636681),
+    posts: [],
+    __v: 4,
+  },
+  {
+    _id: ObjectId('6260b23d4aa3cc38026b76d3'),
+    username: 'minhtran',
+    email: 'lkjfsdlkfdsj@gmail.com',
+    bio: 'My name is Minh',
+    password: '$2a$10$/N99qA1jv5XnclxKJoiNq.9aGJIwPJgBpOoIlt.M2ELYixP1tr5D2',
+    createdAt: new Date(1650504253816),
+    posts: [
+      {
+        _id: ObjectId('6260b313b46ec4f38ee724c1'),
+      },
+    ],
+    __v: 1,
   },
 ]
 
